bea55566ce3d9c29828a823b3cc8388e44532c0d
Alex <alexsantorineos@gmail.com>
2015-05-14 14:11:36 -0500

    Initial Commit

---
+++ ticTacToe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ticTacToe
+//
+//  Created by Alex Santorineos on 5/14/15.
+//  Copyright (c) 2015 madApperz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ticTacToe/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ticTacToe
+//
+//  Created by Alex Santorineos on 5/14/15.
+//  Copyright (c) 2015 madApperz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
